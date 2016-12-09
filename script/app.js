3a47ad4de6e7a57eb4ff5902914218e80e31f560
Daniel Hug <daniel@hugwebdesign.com>
2016-12-08 18:26:56 -0800

    It works

--- script/app.js
+++ script/app.js
@@ -6,15 +6,21 @@ var app = {
 on(qs('#input-form'), 'submit', function inputFormSubmit(event) {
 	event.preventDefault();
 	app.phrase = this['phrase-input'].value.trim();
-	if (app.wordMap) {
+	if (app.dictionary) {
 		handlePhraseSubmission();
 	}
 });
 
 function handlePhraseSubmission() {
+	// get pronunciations for phrase
 	app.phrasePronunciations = getPhrasePronunciations(app.phrase);
 	console.log('Possible phrase pronunciations:', app.phrasePronunciations);
-	generateHeterographs(app.phrasePronunciations[0]);
+
+	// generate heterographs
+	var phonemes = app.phrasePronunciations[0].split(' ');
+	var wordMap = generateWordMap(phonemes);
+	var heterographs = getHeterographsStartingAt(wordMap, phonemes, 0);
+	console.log('heterographs:\n\n' + heterographs.join('\n'));
 }
 
 // fetch dictionary
@@ -23,8 +29,8 @@ var promiseOfDictionary = fetchDictionary().then(parseDictionary);
 // build pronunciation tree
 promiseOfDictionary.then(buildPronunciationTree);
 
-// build word map
-promiseOfDictionary.then(buildWordMap)
+// build dictionary
+promiseOfDictionary.then(buildDictionary)
 	// generate heterograph for the phrase if already submitted
 	.then(function() {
 		if (app.phrase) {
@@ -69,8 +75,8 @@ function buildPronunciationTree(lines) {
 		console.time('generate pronunciation tree');
 		var pronunciationTree = {};
 		lines.forEach(function(line) {
-			var phonemes = line.split(' ');
-			var word = phonemes.shift();
+			var word = line.word;
+			var phonemes = line.phonemes;
 			var pointer = pronunciationTree;
 
 			// form a branch on pronunciationTree made up of a property for each phoneme
@@ -102,38 +108,68 @@ function buildPronunciationTree(lines) {
 	});
 }
 
-// FIX: some words end in (1), (2), etc. for alternate pronunciations
-function buildWordMap(lines) {
-	console.time('generate word map');
-	var wordMap = {};
+function buildDictionary(lines) {
+	console.time('build dictionary');
+	var dictionary = {};
 	lines.forEach(function(line) {
-		var phonemes = line.split(' ');
-		var word = phonemes.shift().split('(')[0];
+		var word = line.word;
+		var phonemes = line.phonemes;
 		var pronunciation = phonemes.join(' ');
 		// must use hasOwnProperty to avoid false positive on .constructor
-		if (has(wordMap, word)) {
-			wordMap[word].push(pronunciation);
+		if (has(dictionary, word)) {
+			// make sure the entry doesn't already have this pronunciation:
+			// after removing the stress markers some pronunciations are identical
+			if (dictionary[word].indexOf(pronunciation) < 0) {
+				dictionary[word].push(pronunciation);
+			}
 		} else {
-			wordMap[word] = [pronunciation];
+			dictionary[word] = [pronunciation];
 		}
 	});
-	console.timeEnd('generate word map');
-	app.wordMap = wordMap;
-	return wordMap;
+	console.timeEnd('build dictionary');
+	app.dictionary = dictionary;
+	return dictionary;
 }
 
+// Keep a copy of the dictionary and its version in localstorage
 function cacheDictionary(text) {
 	localStorage.setItem('DICTIONARY_VERSION', app.DICTIONARY_VERSION);
 	localStorage.setItem('DICTIONARY_CACHE', text);
 }
 
+/*
+	parse the text passed and return an array with an object for each line
+	
+	sow S AW1
+	sow(2) S OW1
+
+	e.g., the two lines above would produce the following:
+
+	{
+	  word: 'sow',
+	  phonemes: ['S', 'AW']
+	}, {
+	  word: 'sow',
+	  phonemes: ['S', 'OW']
+	}
+*/
 function parseDictionary(text) {
 	// split lines
 	var lines = text.trim().split(/\r?\n/).map(function(line) {
 		// remove comments
 		line = line.split('#')[0];
 
-		return removeStressMarkers(line.split(' '));
+		// phonemes are seperated by spaces
+		var phonemes = line.split(' ');
+
+		return {
+			// the word is before the first space and before the
+			// (1), (2), etc. in entrys of alternate pronunciations
+			word: phonemes.shift().split('(')[0],
+
+			// remove the 0, 1, or 2 after phonemes denoting stress
+			phonemes: removeStressMarkers(phonemes)
+		};
 	});
 
 	console.log('# lines: ' + lines.length);
@@ -141,60 +177,119 @@ function parseDictionary(text) {
 	return lines;
 }
 
-function generateHeterographs(pronunciation) {
-	var phonemes = pronunciation.split(' ');
-	var possibleStartingWords = [];
 
-	// loop through each consecutive phoneme in the phrase
+function generateHeterographsAtPosition(phonemes, position) {
+	var words = [];
+
+	// loop through each consecutive phoneme in the phrase until there
+	// are no words in pronunciationTree starting with those phonemes
 	var curBranch = app.pronunciationTree;
 	for (var i = 0; i < phonemes.length; i++) {
-		curBranch = curBranch[phonemes[i]];
+		curBranch = curBranch[phonemes[position + i]];
 		if (!curBranch) break;
 		if (curBranch.words) {
-			possibleStartingWords.push.apply(possibleStartingWords, curBranch.words)
+			words.push({
+				phonemeCount: i + 1,
+				words: curBranch.words
+			});
 		}
 	}
 
-	console.log('Possible words to start the rebus:', possibleStartingWords);
+	return words;
 }
 
-/*  edge cases and false positives:
 
-	fail:
-	crazy punctuation: interobang, etc.
-	Dr. Drake (doesn't include period in word)
-	fifty-five
+/*
+	// A break-down of the word 'amazing':
 
-	pass:
-	Tennis, soccer, baseball, etc., are outdoor games.
-	front-end
-	He will win- it's obvious!
-*/
-var getWords = (function() {
-	// word seperators: punctuation other than dash and period
-	var re = /[,\/#!?$%\^&\*;:{}=\_—`~() ]+/g;
+	// Each key in wordPositions points to an object.
+	var wordPositions = {
 
-	return function getWords(phrase) {
-		phrase = phrase.toLowerCase();
+	  // Each of these keys is the index in the phrase pronunciation
+	  // of the first phoneme of the words listed in the object it points to
+	  0: {
 
-		var words = phrase.split(re).map(function(word) {
-			// special handling of dash and period as they may appear mid-word
-			return word.slice(-1) === '-' ? word.slice(0,-1) :
-			word[0] === '-' ? word.slice(1) :
-			word.split('.').length > 3 ? word :
-			word.slice(-1) === '.' ? word.slice(0,-1) : word;
-		});
+	    // Each key in these objects is the length of the words
+	    // (number of phonemes) in the array they point to.
+	    1: ['a', 'uh'],
+	    2: ['uhm'],
+	    4: ['amaze']
+	  },
 
-		// don't include last empty word resulting from closing sentence punctuation
-		return words[words.length - 1] === '' ? words.slice(0, -1) : words;
+	  1: {
+	    2: ['may'],
+	    3: ['maze', 'maize']
+	  },
+
+	  3: {
+	    3: ['zing']
+	  }
 	};
-})();
+*/
+function generateWordMap(phonemes) {
+	var wordMap = {};
 
+	// get an array of objects containing all the words that are pronounced at the
+	// front of the phrase: each object contains words with a different phoneme count
+	generateHeterographsAtPosition(phonemes, 0).forEach(function addWords(heterographsOfALength) {
+		var position = this;
+		var phonemeCount = heterographsOfALength.phonemeCount;
+		var words = heterographsOfALength.words;
+
+		// place each word in wordMap cataloged by their
+		// position (index of first phoneme) and phoneme count
+		if (!wordMap[position]) {
+			wordMap[position] = {};
+		}
+		wordMap[position][phonemeCount] = words;
+
+		// run again at the position following each word
+		var newPosition = position + phonemeCount;
+		if (newPosition < phonemes.length && !wordMap[newPosition]) {
+			generateHeterographsAtPosition(phonemes, newPosition)
+				// pass the index (in phonemes) of the next word as the this value
+				.forEach(addWords, newPosition);
+		}
+	}, 0);
+
+	console.log('Heterograph word map:', wordMap);
+	return wordMap;
+}
+
+
+/*
+*/
+function getHeterographsStartingAt(wordMap, phonemes, position) {
+	var phrases = [];
+
+	// loop through the wordsOfLength arrays at position 0
+	var wordsAtPos = wordMap[position];
+	if (!wordsAtPos) return [];
+	for (var phonemeCount in wordsAtPos) {
+		phonemeCount = +phonemeCount;
+		// get the words in each
+		var words = wordsAtPos[phonemeCount];
+
+		// if there are no more words
+		var nextWordPos = position + phonemeCount;
+		if (nextWordPos >= phonemes.length) return words;
+		var heterographsAfterThis = getHeterographsStartingAt(wordMap, phonemes, nextWordPos);
+		if (heterographsAfterThis.length)
+		var newPhrases = combineStrings(words, heterographsAfterThis);
+		phrases.push.apply(phrases, newPhrases);
+	}
+
+	return phrases;
+}
+
+
+// return an array of phoneme-combo strings each representing
+// an alternate pronunciation of the passed phrase
 function getPhrasePronunciations(phrase) {
 	var wordsAfterFirst = getWords(phrase);
 	console.log('Words entered: ', wordsAfterFirst);
 	var firstWord = wordsAfterFirst.shift();
-	var firstWordPronunciations = app.wordMap[firstWord]
+	var firstWordPronunciations = app.dictionary[firstWord];
 
 	// seed the array of possible pronunciations of the
 	// phrase with possible pronunciations of the first word
@@ -204,26 +299,18 @@ function getPhrasePronunciations(phrase) {
 	// phrase with the rest of the words including every
 	// combination of the different pronciations of each word
 	wordsAfterFirst.forEach(function(word) {
-		var wordPronunciations = app.wordMap[word] || [''];
+		var wordPronunciations = app.dictionary[word] || [''];
 
 		// concatenate the arrays of "possible pronunciations
 		// of the phrase up to this word". Each array assumes
 		// a different pronunciation for the current word
-		phrasePronunciations = [].concat.apply([], wordPronunciations.map(function(wordPronunciation) {
-			// add this pronunciation of the word to the end of every
-			// possible pronunciation for the phrase up to this point in order
-			// to get a new array of possible pronunciations of the phrase
-			// up to this word assuming this pronunciation for the word
-			var phrasePronunciationsWithThisWordPronunciation = 
-				phrasePronunciations.map(function(phrasePronunciation) {
-					return phrasePronunciation + ' ' + wordPronunciation;
-				});
-			return phrasePronunciationsWithThisWordPronunciation;
-		}));
+		phrasePronunciations = combineStrings(phrasePronunciations, wordPronunciations);
 	});
 	return phrasePronunciations;
 }
 
+// remove the digits 0, 1, and 2 from each
+// of the phoneme strings in the passed array
 function removeStressMarkers(phonemes) {
 	return phonemes.map(function(phoneme) {
 		var lastChar = phoneme[phoneme.length - 1];
@@ -231,5 +318,15 @@ function removeStressMarkers(phonemes) {
 			return phoneme.slice(0,-1);
 		}
 		return phoneme;
-	}).join(' ');
+	});
+}
+
+// return all combinations of a string in firstStrings plus a string in secondStrings
+function combineStrings(firstStrings, secondStrings) {
+	return [].concat.apply([], firstStrings.map(function(aFristString) {
+		// return strings that use this first string
+		return secondStrings.map(function(aSecondString) {
+			return aFristString + ' ' + aSecondString;
+		});;
+	}));
 }
--- script/helpers.js
+++ script/helpers.js
@@ -10,3 +10,39 @@ function on(target, type, callback, useCapture) {
 
 // safe hasOwnProperty: has(obj, prop)
 var has = Function.prototype.call.bind(Object.prototype.hasOwnProperty);
+
+/*
+	return an array of the words in the passed
+	string lowercased and stripped of punctuation
+
+	edge cases and false positives:
+
+	fail:
+	crazy punctuation: interobang, etc.
+	Dr. Drake (doesn't include period in word)
+	fifty-five
+
+	pass:
+	Tennis, soccer, baseball, etc., are outdoor games.
+	front-end
+	He will win- it's obvious!
+*/
+var getWords = (function() {
+	// word seperators: punctuation other than dash and period
+	var re = /[,\/#!?$%\^&\*;:{}=\_—`~() ]+/g;
+
+	return function getWords(phrase) {
+		phrase = phrase.toLowerCase();
+
+		var words = phrase.split(re).map(function(word) {
+			// special handling of dash and period as they may appear mid-word
+			return word.slice(-1) === '-' ? word.slice(0,-1) :
+			word[0] === '-' ? word.slice(1) :
+			word.split('.').length > 3 ? word :
+			word.slice(-1) === '.' ? word.slice(0,-1) : word;
+		});
+
+		// don't include last empty word resulting from closing sentence punctuation
+		return words[words.length - 1] === '' ? words.slice(0, -1) : words;
+	};
+})();
